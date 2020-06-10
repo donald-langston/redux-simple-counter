587ab7d30d20ba0d0413e9fe3220f8652afdc452
Donald <donald_langston@yahoo.com>
2020-06-09 23:10:17 -0400

    Added decrement, add by 5 and subtract by 5 buttons

--- src/index.js
+++ src/index.js
@@ -9,4 +9,16 @@ subscribe(() => render(getState()))
 
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+const incrementBy5Btn = document.getElementById('increment-5');
+const decrementBy5Btn = document.getElementById('decrement-5');
 incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+decrementButton.addEventListener('click', () => {
+    dispatch({type: "DECREMENT"});
+});
+incrementBy5Btn.addEventListener('click', () => {
+    dispatch({type: "INCREMENT_BY_FIVE"});
+});
+decrementBy5Btn.addEventListener('click', () => {
+    dispatch({type: "DECREMENT_BY_FIVE"});
+});
--- src/reducer.js
+++ src/reducer.js
@@ -8,6 +8,18 @@ const reducer = (state = initialState, action) => {
         return {
             value: state.value + 1
         }
+    } else if(type === "DECREMENT") {
+        return {
+            value: state.value - 1
+        }
+    } else if(type === "INCREMENT_BY_FIVE") {
+        return {
+            value: state.value + 5
+        }
+    } else if(type === "DECREMENT_BY_FIVE") {
+        return {
+            value: state.value - 5
+        }
     }
     return state
 }
